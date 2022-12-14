e3076534f3e7e437555c97dea649df7bbff29908
arvandha121 <nauvan121@gmail.com>
2022-12-15 01:35:38 +0700

    asd

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,10 @@ use Illuminate\Support\Facades\Artisan;
 */
 
 Route::get('/mysql', function () {
-    // Artisan::call('migrate:rollback', ['--force' => true]);
+    Artisan::call('migrate:rollback', ['--force' => true]);
     // Artisan::call('migrate', ['--force' => true]);
-    Artisan::call('migrate --env=production');
-    // Artisan::call('db:seed', ['--force' => true]);
+    // Artisan::call('migrate --env=local');
+    Artisan::call('db:seed', ['--force' => true]);
     // Artisan::call('db:seed');
 });
 
